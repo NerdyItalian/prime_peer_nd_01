7824e9a12ba52d6d8e6afce5b36fde97ea136b1e
NerdyItalian <atava8384@gmail.com>
2015-05-06 15:50:58 -0500

    Changed port

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 
 var conversions = require('./conversion');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', 5000);
 
 app.get('/',function(request, response){
-    //response.sendStatus(conversions.accountBalance() + conversions.randomMoney());
+    response.sendStatus(conversions.accountBalance() + conversions.randomMoney());
 });
 
 app.listen(app.get('port'), function(){
